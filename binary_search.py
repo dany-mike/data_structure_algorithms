c9bc1d19b86b688253b17c112fe350fe80b2fe8f
Dany <danymike.joseph@gmail.com>
2023-01-02 19:28:20 +0100

    feature: binary search made from scratch

--- binary_search.py
+++ binary_search.py
@@ -13,8 +13,7 @@ doesn't exist in the list."""
 
 def binary_search(input_array, value):
     iterations = 0
-    compute_array = input_array.copy()
-    # 2³ = 8 which is greater than test_list length so I only need 3 iterations to resolve the problem
+    compute_array = input_array[:]
     while iterations < 3:
         iterations += 1
         currentMiddle = findMiddle(compute_array)
@@ -27,12 +26,14 @@ def binary_search(input_array, value):
             compute_array = compute_array[currentMiddleIndex + 1::]
     return -1
 
+
 def findMiddle(input_list):
     middle = float(len(input_list))/2
     if middle % 2 != 0:
         return input_list[int(middle - .5)]
     else:
         return (input_list[int(middle)], input_list[int(middle-1)])
+
 
 test_list = [1, 3, 9, 11, 15, 19, 29]
 test_val1 = 25
